e03f30c0c69d5c9a2ea7635a90c90e4d1cc97fa1
Abhishek Sunkum Rammurthy <sunkum@efr.de>
2025-12-08 09:50:36 +0100

    Applet loading working with secure mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
-
-./sam-ba -l verbose:all -p secure:ttyACM0 -d sama5d35 -a nandflash:1:8:0xc1e04e07
+echo "Starting udevd..."
+/lib/systemd/systemd-udevd --daemon
+echo "Triggering udev events..."
+udevadm trigger
+echo "Running sam-ba..."
+./sam-ba -p secure:ttyACM0 -d sama5d35 -a nandflash:1:8:0xc1e04e07
